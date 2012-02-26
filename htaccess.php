c663d061486d326212be7fced33ecb1703e7548c
Fabian Freiburg <f.freiburg@googlemail.com>
2012-02-26 19:51:13 +0100

    Cleaned up the indentation

--- htaccess.php
+++ htaccess.php
@@ -31,20 +31,20 @@ $filters = array(".svn", ".cvs", ".git");
 //Merge base set of filters with any from the command line
 if(count($argv) > 0)
 {
-   unset($argv[0]);
-   $filters = array_merge($filters, $argv);
+    unset($argv[0]);
+    $filters = array_merge($filters, $argv);
 }
 
 //Iterate recursively through everything from here on in, of course filtering out stuff from the filter list
 foreach (new fileFilter(new RecursiveIteratorIterator($ite), $filters) as $filename=>$cur)
 {
-   $htaccessFiles[] = $filename;
+    $htaccessFiles[] = $filename;
 }
 
 //No files? Quit now!
 if (count($htaccessFiles) == 0)
 {
-   die("No .htaccess files found");
+    die("No .htaccess files found");
 }
 
 //Sort the list, place least depth first. This is important to allow overrides from sub-directories to occur correctly
@@ -56,36 +56,36 @@ $flags = 0;
 //Iterate over found files (sorted now) and read them in.
 foreach($htaccessFiles as $file)
 {
-   //Grab the file and print out the <Directory $path> bit
-   $path = realpath(pathinfo($file, PATHINFO_DIRNAME));
-   echo "<Directory $path>\n";
-   $lines = file($file);
-   if(count($lines) > 0)
-   {
-		//Tab the file in, check for RedirectBase which may cause problems
-       foreach($lines as $line)
-       {
-           echo "\t$line";
-           if(stripos($line, "RedirectBase") !== FALSE)
-           {
-	       //Not tabbed! See what happens there?
-               echo "# WARNING The above line contains RedirectBase which may not convert directly to a conf file. Please check manually\n";
-               $flags++;
-           }
-       }
-      
-       //Handle issues where files don't end with a newline
-       if (in_array(substr($line, -1), array("\n", "\r")))
-       {
-           echo "</Directory>\n\n";
-       }else
-       {
-           echo "\n</Directory>\n\n";
-       }
-   }else{
-       //File was empty, leave the stub in
-       echo "\n</Directory>\n\n";
-   }
+    //Grab the file and print out the <Directory $path> bit
+    $path = realpath(pathinfo($file, PATHINFO_DIRNAME));
+    echo "<Directory $path>\n";
+    $lines = file($file);
+    if(count($lines) > 0)
+    {
+        //Tab the file in, check for RedirectBase which may cause problems
+        foreach($lines as $line)
+        {
+            echo "\t$line";
+            if(stripos($line, "RedirectBase") !== FALSE)
+            {
+                //Not tabbed! See what happens there?
+                echo "# WARNING The above line contains RedirectBase which may not convert directly to a conf file. Please check manually\n";
+                $flags++;
+            }
+        }
+
+        //Handle issues where files don't end with a newline
+        if (in_array(substr($line, -1), array("\n", "\r")))
+        {
+            echo "</Directory>\n\n";
+        }else
+        {
+            echo "\n</Directory>\n\n";
+        }
+    }else{
+        //File was empty, leave the stub in
+        echo "\n</Directory>\n\n";
+    }
 }
 
 //Check for warnings
@@ -105,17 +105,17 @@ echo "# Please test before going live, no guarantees! \n";
 //Sort by the number of path segments, least first
 function sorter($a, $b)
 {
-   $a = count(explode("/", $a));
-   $b = count(explode("/", $b));
-   if($a == $b)
-   {
-       return 0;
-   }
-   if($a > $b)
-   {
-       return 1;
-   }
-   return -1;
+    $a = count(explode("/", $a));
+    $b = count(explode("/", $b));
+    if($a == $b)
+    {
+        return 0;
+    }
+    if($a > $b)
+    {
+        return 1;
+    }
+    return -1;
 }
 
 
@@ -123,30 +123,30 @@ function sorter($a, $b)
 //Filter out specified files. 
 class fileFilter extends FilterIterator
 {
-   private $filters;
-   public function __construct(Iterator $iterator, $filters)
-   {
-       parent::__construct($iterator);
-       $this->filters = $filters;
-   }
+    private $filters;
+    public function __construct(Iterator $iterator, $filters)
+    {
+        parent::__construct($iterator);
+        $this->filters = $filters;
+    }
 
-   public function accept()
-   {
-       $dir = $this->getInnerIterator()->current();
-       foreach($this->filters as $filter)
-       {
-           if(strpos($dir, $filter) !== false)
-           {
-               return false;
-           }
-       }
-       
-       $filename = DIRECTORY_SEPARATOR . '.htaccess';
-       if (substr($dir, - strlen($filename), strlen($filename)) == $filename)
-       {
-           return true;
-       }
-       //echo "Skipping $dir\n";
-       return false;
-   }
+    public function accept()
+    {
+        $dir = $this->getInnerIterator()->current();
+        foreach($this->filters as $filter)
+        {
+            if(strpos($dir, $filter) !== false)
+            {
+                return false;
+            }
+        }
+
+        $filename = DIRECTORY_SEPARATOR . '.htaccess';
+        if (substr($dir, - strlen($filename), strlen($filename)) == $filename)
+        {
+            return true;
+        }
+        //echo "Skipping $dir\n";
+        return false;
+    }
 }
